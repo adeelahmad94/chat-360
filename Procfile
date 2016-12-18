09123a10d79b57552b9c25fa7a8fea22efde79e4
adeel <axes321@gmail.com>
2016-12-18 15:19:30 +0500

    commit : code updated

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: node peer-server.js
+web: node server.js
---
+++ server.js
@@ -0,0 +1,4 @@
+var PeerServer = require('./lib/server').PeerServer;
+var server = new PeerServer({
+	port: process.env.PORT || 9000, key: 'peerjs'
+});
