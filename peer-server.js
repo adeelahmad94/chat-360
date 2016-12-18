70fe9df7c19a253b46ea04a498517a12bf734155
adeel <axes321@gmail.com>
2016-12-18 14:51:34 +0500

    commit 2

--- peer-server.js
+++ peer-server.js
@@ -1,14 +1,6 @@
 var PeerServer = require('peer').PeerServer;
-var server = PeerServer({port: 9000, path: '/peerjs'});
-
-//var fs = require('fs');
-//var PeerServer = require('peer').PeerServer;
-//
-//var server = PeerServer({
-//    port: 9000,
-//    ssl: {
-//        key: fs.readFileSync('/path/to/your/ssl/key/here.key'),
-//        cert: fs.readFileSync('/path/to/your/ssl/certificate/here.crt')
-//    }
-//});
+var server = new PeerServer({
+    port: process.env.PORT || 9000,
+    path: 'peerjs'
+});
 
